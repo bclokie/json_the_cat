1d69d2017377e2be5dc4686cabbeddd592421d02
Braden <braden.clokie@gmail.com>
2023-01-20 18:08:31 -0500

    Refactored breedFetcher and added index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,23 @@
 const request = require('request');
 
-const breedName = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+const fetchBreedDescription = (breedName, callback) => {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
+    if (error) {
+      console.log(`Error: ${error}`);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      console.log(`Error: Invalid status code ${response.statusCode}`);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      console.log(`Error: Breed ${breedName} not found`);
+      return;
+    }
+    callback(null, data[0].description);
+  });
+};
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(`Error: ${error}`);
-    return;
-  }
-  if (response.statusCode !== 200) {
-    console.log(`Error: Invalid status code ${response.statusCode}`);
-    return;
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log(`Error: Breed ${breedName} not found`);
-    return;
-  }
-  console.log(data[0].description);
-});
+module.exports = { fetchBreedDescription };
